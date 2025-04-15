49df6028f80bd5f89f76549d3b55a607f5b64e92
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-04-16 01:07:07 +0300

    Refactoring model.js

--- js/model.js
+++ js/model.js
@@ -1,30 +1,33 @@
-let fruits = ['banane', 'apple', 'pineapple']
+let fruits = []
 
 function addFruit(fruit) {
   fruits.push(fruit)
-}
-
-function removeFruit(fruit) {
-  if (!fruits.includes(fruit)) return
-  const idx = fruits.indexOf(fruit)
-  fruits.splice(idx, 1)
 }
 
 function cleanFruits() {
   fruits = []
 }
 
+function removeFruit(fruit) {
+  const idx = getIndexByFruitText(fruit)
+  fruits.splice(idx, 1)
+}
+
 function editFruitText(newText, oldText) {
-  if (!fruits.includes(oldText)) return
-  const idx = fruits.indexOf(oldText)
+  const idx = getIndexByFruitText(oldText)
   fruits.splice(idx, 1, newText)
 }
 
-addFruit('mango')
-fruits
-removeFruit('mango')
-fruits
-cleanFruits(fruits)
-fruits
-editFruitText('gg')
-fruits
+function getIndexByFruitText(fruitText) {
+  if (!fruits.includes(fruitText)) return
+  return fruits.indexOf(fruitText)
+}
+
+// addFruit('mango')
+// fruits
+// removeFruit('mango')
+// fruits
+// cleanFruits(fruits)
+// fruits
+// editFruitText('gg')
+// fruits
